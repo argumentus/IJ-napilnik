a5f9506e7d75fcf9a2a45c6653add47fd170655a
argument <imbagw@gmail.com>
2021-10-02 20:39:59 +0300

    Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task09
+{
+    internal class Program
+    {
+        // Перед вами оружие - https://gist.github.com/HolyMonkey/1cc8afb9001dc20c58731a0468d28936
+        
+        class Gun
+        {
+            public void Shoot() { }
+        }
+        
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
