65603a9fbc876234231d9dda1d931956799f3644
argument <imbagw@gmail.com>
2021-10-02 15:01:40 +0300

    Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task07
+{
+    internal class Program
+    {
+        // Измените код - https://gist.github.com/HolyMonkey/fece8ba4657cb3583ae74949b252d26a
+        
+        class Weapon
+        {
+            private readonly int _countBulletsForShoot;
+            private int _bullets;
+
+            public Weapon(int countBulletsForShoot)
+            {
+                _countBulletsForShoot = countBulletsForShoot;
+            }
+
+            public bool CanShoot() => _bullets - _countBulletsForShoot > 0;
+
+            public void Shoot()
+            {
+                if (!CanShoot())
+                    throw new ArgumentException(nameof(CanShoot));
+                    
+                _bullets -= _countBulletsForShoot;
+            }
+        }
+        
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
