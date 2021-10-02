349096f8e0ca401c5221d52b2f459b0717fe6a32
argument <imbagw@gmail.com>
2021-10-02 15:29:13 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Task10
+{
+    internal class Program
+    {
+        // Переименуйте классы - https://gist.github.com/HolyMonkey/301475a9d6ec900584a254cee3485673
+        
+        class Player { }
+        class Gun { }
+        class Target { }
+        class Unit
+        {
+            public IReadOnlyCollection<Unit> List {get; private set;}
+        }
+        
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
