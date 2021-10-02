cca7a481a62992ca6d94837689bd81302d61dd00
argument <imbagw@gmail.com>
2021-10-02 14:37:55 +0300

    Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task05
+{
+    internal class Program
+    {
+        // Переименуйте метод - https://gist.github.com/HolyMonkey/92e8c9c2f08471e31eceef30da7ea6ad
+        
+        public static int ComparisonNumber(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+        
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
