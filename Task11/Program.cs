7a99104a49c9bac0bd3431a2c5978083cd9ea8cb
argument <imbagw@gmail.com>
2021-10-02 15:38:04 +0300

    Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task11
+{
+    internal class Program
+    {
+        public static void CreateObject()
+        {
+            //Создание объекта на карте
+        }
+
+        public static void RandomizeChance()
+        {
+            _chance = Random.Range(0, 100);
+        }
+
+        public static int CalculateSalary(int hoursWorked)
+        {
+            return _hourlyRate * hoursWorked;
+        }
+        
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
