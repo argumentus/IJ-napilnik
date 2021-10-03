ccd910fb0fbf51891ca1daed596cebed488391a3
argument <imbagw@gmail.com>
2021-10-03 13:51:06 +0300

    Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Task16
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            public void Shoot(Player player) {}
+            public string FindByIndex(int number) {}
+        }
+    }
+}
